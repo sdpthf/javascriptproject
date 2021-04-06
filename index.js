59503f69f9aed06ca7ac5586ed91797d9bdccff4
김범진 <sdpthf@naver.com>
2021-04-06 18:53:53 +0900

    [MODIFY] index.js js http.server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+    res.write("Hello World");
+    res.end();
+});
+
+app.listen(3000, () => {
+    console.log("server on http://localhost:3000");
+});
